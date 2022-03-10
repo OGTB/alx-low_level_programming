4da5726d2362e130212c2dd332129f5e57c7fcd8
OGTB <ogaatonny@gmail.com>
2022-03-10 06:39:27 -0800

    put

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
+
+/**
+ * put
+ */
 int main(void)
-	/**
-	 * main
-	 * put
-	 * return
-	 * /
 {
-puts("\"Programming is like building a multilingual puzzle")
-	return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
