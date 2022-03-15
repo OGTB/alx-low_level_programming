5d80915cb0d8d70afc56a5ffecca71e0659fe38b
OGTB <ogaatonny@gmail.com>
2022-03-15 03:04:42 -0700

    check for c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * main - entry point
+ * Description: check for lowerCase @c
+ * @c: the character to check
+ * Return: 1 if lowerCase and if otherwise
+ */
+
+int _islower(int c);
+{
+
+	if (c >= 'z' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+
+	_putchar('\n')
+}
