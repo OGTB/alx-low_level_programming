a3f8ad8eb7e166743cce2cc66dc08d887ea846ef
OGTB <ogaatonny@gmail.com>
2022-03-15 02:22:50 -0700

    lowerCase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Description: print
+ * Return 0
+ */
+void print_alphabet(void);
+{
+	char a;
+
+	a = 'a';
+
+	while (a <= 'z)
+	_putchar(a);
+	a++;
+	_putchar('\n')
+}
