17847c20945ba0065cc1fc25ca887e72ef28e8b1
OGTB <ogaatonny@gmail.com>
2022-03-15 07:43:47 -0700

    absolute integer

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,13 +2,10 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: n is an integer
- * int: the integer to check
- * Description: prints the absolute value of an integer
+ * @n: the int to check
  * Return: the absolute value of int
  */
-int _abs(int)
-
+int _abs(int n)
 {
 	if (n >= 0)
 	{
