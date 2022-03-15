9e6d4b7f1112a49ddf369ef6c60b722b8ae01c80
OGTB <ogaatonny@gmail.com>
2022-03-15 06:11:38 -0700

    prints the last digit

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: n is an integer
+ * int: the integer to check
+ * Description: prints the absolute value of an integer
+ * Return: the absolute value of int
+ */
+int _abs(int)
+
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	return (-n);
+}
