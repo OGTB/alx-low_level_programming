fc4cfe9215a3ea12115ae44afae3d113996fbb70
OGTB <100015358+OGTB@users.noreply.github.com>
2022-03-17 23:32:41 +0300

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,17 @@
 #include "main.h"
-
 /**
- * print_most_numbers - prints the numbers, from 0 to 9,
- * followed by a new line
+ * print_numbers - Print numbers, from 0 to 9
+ *
+ * Return: 0
  */
-
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	int i;
+	int i = 48;
 
-	for (i = 0; i < 10; i++)
+	while (i <= 57)
 	{
-		if (i != 2 && i != 4)
-		{
-			_putchar(i + '0');
-		}
+		_putchar(i);
+		i++;
 	}
-
 	_putchar('\n');
 }
