e9e23250e5a4760206ecad95820b5b4094a0eb20
OGTB <ogaatonny@gmail.com>
2022-03-17 03:57:30 -0700

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: is to be checked
+ * Return: 1 if successful, otherwise 0
+ */
+
+int _isupper(int c);
+{
+	int c;
+
+	if (isupper(c))
+	{
+		return (1);
+	}
+
+	else
+
+	{
+		return (0);
+	}
+}
