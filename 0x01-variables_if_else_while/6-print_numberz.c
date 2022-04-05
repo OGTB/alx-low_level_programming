9ae16b5f4168104091fbdcc82bfb98dc8eabb426
OGTB <100015358+OGTB@users.noreply.github.com>
2022-04-05 22:24:11 +0300

    digit numbers of base 10 starting from 0

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
-
 /**
- * main - prints all single digit numbers of base 10 starting from 0,
- * followed by a new line, using putchar
+ * main - prints all single digits numbers of base 10
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-	for (n = 11; n < 21; n++)
+	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
