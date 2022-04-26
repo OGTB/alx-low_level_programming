49fbf0e2d2a783fdcfcf47438f4dd56e8c967df8
OGTB <ogaatonny@gmail.com>
2022-04-26 16:46:02 -0700

    returns nodes

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,23 @@
+
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * get_nodeint_at_index - Find a node in a list
+ * @head: Address of the first node in a list
+ * @index: Position of the node to find
+ * Return: NULL if the node does not exist
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i = 0;
+
+	if (head == NULL)
+		return (NULL);
+	for (i = 0; i < index; y++)
+	{
+		head = head->next;
+		if (head == NULL)
+			return (NULL);
+	}
+	return (head);
+}
